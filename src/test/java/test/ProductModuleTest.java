549de938cdc525b2c1a8b04c66c8cd6189302458
曾轼麟 <zengshilin@qipeipu.com>
2019-07-17 17:40:15 +0800

    修改项目名称和 Readme

--- src/test/java/test/ProductModuleTest.java
+++ src/test/java/test/ProductModuleTest.java
@@ -5,9 +5,6 @@ import custom.client.kafka.config.KafkaProducerConfig;
 import custom.client.kafka.producer.MyKafkaProducer;
 
 import java.util.UUID;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @program: kafka-test
@@ -16,17 +13,6 @@ import java.util.concurrent.TimeUnit;
  * @create: 2019-07-09 12:25
  **/
 public class ProductModuleTest {
-
-    static ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(
-            8,
-            //CPU密集型任务 核心数量 * 2 + 1（备份线程） maximumPoolSize 不要过多，过多也是进入阻塞队列里面
-            9,
-            //不等待
-            0L,
-            TimeUnit.MILLISECONDS,
-            //允许等等队列大小 （运维脚本等待时间/每个任务执行的时间 = 任务等待队列长度）
-            new LinkedBlockingQueue<>(8)
-    );
 
     static MyKafkaProducer producer = new MyKafkaProducer(KafkaProducerConfig.builder()
             .bootstrapServers("kafka-service:9092,kafka-service2:9092,kafka-service3:9092")
